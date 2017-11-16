508a6972b68e0f7da55af1dea8be8bdca1cd718b
veryhumble <janhumbel@gmail.com>
2017-11-16 11:19:23 +0100

    try

--- appveyor_install.ps1
+++ appveyor_install.ps1
@@ -1,5 +1,9 @@
 $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\android.bat"
 $AndroidSdkManagerToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\sdkmanager.bat"
 
+if (!(Test-Path $AndroidToolPath)) {
+    $AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android.bat"
+}
 
-$AndroidSdkManagerToolPath --install platform-tools,build-tools-26.0.1,build-tools-25.0.2,android-25,android-24,extra-android-m2repository,extra-google-m2repository
+
+$null = Echo 'y' | & $AndroidToolPath update sdk -u -a -t platform-tools,build-tools-26.0.1,build-tools-25.0.2,android-25,android-24,extra-android-m2repository,extra-google-m2repository
