37c5bd877619e12afd77fcdfab80c4d913fb6449
veryhumble <janhumbel@gmail.com>
2017-11-16 11:16:19 +0100

    test using sdmanager

--- appveyor_install.ps1
+++ appveyor_install.ps1
@@ -1,39 +1,5 @@
 $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\android.bat"
+$AndroidSdkManagerToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\sdkmanager.bat"
 
-if (!(Test-Path $AndroidToolPath)) {
-    $AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android.bat"
-}
 
-Function Get-AndroidSDKs() {
-    $output = & $AndroidToolPath list sdk --all
-    $sdks = $output |% {
-        if ($_ -match '(?<index>\d+)- (?<sdk>.+), revision (?<revision>[\d\.]+)') {
-            $sdk = New-Object PSObject
-            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Index -Value $Matches.index
-            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Name -Value $Matches.sdk
-            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Revision -Value $Matches.revision
-            $sdk
-        }
-    }
-    $sdks
-}
-
-Function Install-AndroidSDK() {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory=$true, Position=0)]
-        [PSObject[]]$sdks
-    )
-
-    $sdkIndexes = $sdks |% { $_.Index }
-    $sdkIndexArgument = [string]::Join(',',  $sdkIndexes)
-    Write-Output "Installing additional Android SDKs..."
-    $sdks | Format-Table Name
-
-    # Suppress the output to STDOUT
-    $null = Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument
-}
-
-$sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 24*' -or $_.name -like 'google apis*api 24' }
-
-Install-AndroidSDK -sdks $sdks
+$AndroidSdkManagerToolPath --install platform-tools,build-tools-26.0.1,build-tools-25.0.2,android-25,android-24,extra-android-m2repository,extra-google-m2repository
