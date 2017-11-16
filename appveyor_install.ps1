51310d4c41f5815224c88774dc8821969b2e61d5
veryhumble <janhumbel@gmail.com>
2017-11-16 11:35:51 +0100

    update all

--- appveyor_install.ps1
+++ appveyor_install.ps1
@@ -8,5 +8,6 @@ if (!(Test-Path $AndroidToolPath)) {
 if (!(Test-Path $AndroidSdkManagerToolPath)) {
     $AndroidSdkManagerToolPath = "$env:localappdata\Android\android-sdk\tools\bin\sdkmanager.bat"
 }
+$null = Echo 'y' | & $AndroidToolPath update sdk -u
 
-& $AndroidToolPath update sdk --no-ui --all --filter platform-tools,build-tools-26.0.1,build-tools-25.0.2,android-25,android-24,extra-android-m2repository,extra-google-m2repository
+$null = Echo 'y' | & $AndroidToolPath update sdk --no-ui --all --filter platform-tools,build-tools-26.0.1,build-tools-25.0.2,android-25,android-24,extra-android-m2repository,extra-google-m2repository
